0155d5e56e4f114f04842a91ccdf26c08cec2cce
Thomas Bratt <thomas.bratt@gmail.com>
2021-08-17 16:08:50 +0100

    Update for Fedora 34

---
+++ .bashrc.d/bash_aliases.sh
@@ -0,0 +1,34 @@
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+alias disk='lsblk -o TYPE,NAME,MOUNTPOINT,FSTYPE,SIZE,FSAVAIL,FSUSE%,FSUSED,UUID,PTUUID,PHY-SEC,LOG-SEC'
+alias end='_end(){ hexdump -C "$1" | grep --color -E " 0a| 0d|$"; };_end '
+alias rp=realpath
+
+alias ga='git add'
+alias gc='git checkout'
+alias gd='git diff'
+alias gl='git log --decorate --name-status'
+alias gs='git status'
+
+alias gaa='git add -A'
+alias gba='git branch -a'
+alias gca='git commit --amend'
+alias gcm='git commit -m'
+alias gdc='git diff --cached'
+alias gpo='git push origin'
+alias gpr='git pull --rebase'
+alias gr='git restore'
+alias grs='git restore staged'
+
+alias jb='cd ~/Dropbox;clear;pwd;ls -al'
+alias jd='cd ~/Downloads;clear;pwd;ls -al'
+alias js='cd ~/src;clear;pwd;ls -al'
+alias jt='cd ~/tmp;clear;pwd;ls -al'
+alias la='ls -al'
+alias ll='ls -l'
+alias ls='ls --color=auto -h --time-style=+'\''%Y-%m-%d %H:%M:%S'\'' '
+
+alias sss='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '
+alias ssp='scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '
+
--- update_repository.sh
+++ update_repository.sh
@@ -3,28 +3,29 @@
 # Copy the latest versions of the dotfiles into the git repository.
 
 ALL_DOT_FILES=(
-    '.bash_aliases'
-    '.bash_aliases_work'
-    '.bash_terminal'
+    '.bashrc'
+    '.bashrc.d/bash_aliases.sh'
+    '.bashrc.d/bash_selected_editor.sh'
+    '.bashrc.d/bash_terminal.sh'
     '.gitconfig'
     '.gitignore'
-    '.selected_editor'
     '.ssh/config'
     '.vimrc'
 )
 
 main(){
     for f in "${ALL_DOT_FILES[@]}"; do
-        if cmp -s "$HOME/$f" "$f"; then
-            printf 'Unchanged       : %s\n' "$f"
+        if [[ ! -f ${HOME}/${f} ]]; then
+            printf 'Missing         : %s\n' "${f}"
+        elif cmp -s "${HOME}/${f}" "${f}"; then
+            printf 'Unchanged       : %s\n' "${f}"
         else
-            mkdir -p $(dirname "$f")
-            \cp "$HOME/$f" "$f"
-   
-            if cmp -s "$HOME/$f" "$f"; then
-                printf 'Updated         : %s\n' "$f"
+            mkdir -p $(dirname "${f}")
+            \cp "${HOME}/${f}" "${f}" 2>/dev/null
+            if cmp -s "${HOME}/${f}" "${f}"; then
+                printf 'Updated         : %s\n' "${f}"
             else
-                printf 'Failed to update: %s\n' "$f"
+                printf 'Failed          : %s\n' "${f}"
             fi 
         fi
     done
