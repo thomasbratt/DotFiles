e58eab14d9a43320896cd1408abd49789d706d0f
Thomas Bratt <thomas.bratt@gmail.com>
2022-03-25 19:08:55 +0000

    Sort aliases by name

--- aliases.sh
+++ aliases.sh
@@ -1,36 +1,38 @@
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 alias disk='lsblk -o TYPE,NAME,MOUNTPOINT,FSTYPE,SIZE,FSAVAIL,FSUSE%,FSUSED,UUID,PTUUID,PHY-SEC,LOG-SEC'
+alias egrep='egrep --color=auto'
 alias end='_end(){ hexdump -C "$1" | grep --color -E " 0a| 0d|$"; };_end '
-alias rp=realpath
-
+alias fgrep='fgrep --color=auto'
 alias ga='git add'
-alias gc='git checkout'
-alias gd='git diff'
-alias gl='git log --decorate --name-status'
-alias gs='git status'
-
 alias gaa='git add -A'
 alias gba='git branch -a'
+alias gc='git checkout'
 alias gca='git commit --amend'
 alias gcm='git commit -m'
+alias gd='git diff'
 alias gdc='git diff --cached'
+alias gl='git log --decorate --name-status'
 alias gp='git pull'
 alias gpo='git push origin'
 alias gr='git restore'
+alias grep='grep --color=auto'
 alias grs='git restore --staged'
-
+alias gs='git status'
 alias jb='cd ~/Dropbox;clear;pwd;ls -al'
 alias jd='cd ~/Downloads;clear;pwd;ls -al'
 alias js='cd ~/src;clear;pwd;ls -al'
 alias jt='cd ~/tmp;clear;pwd;ls -al'
+alias l.='ls -d .* --color=auto'
 alias la='ls -al'
 alias ll='ls -l'
 alias ls='ls --color=auto -h --time-style=+'\''%Y-%m-%d %H:%M:%S'\'' '
-
-alias ps='procs --tree'
-
-alias sss='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '
+alias procs='procs --tree'
+alias rp='realpath'
 alias ssp='scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '
-
+alias sss='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '
+alias xzegrep='xzegrep --color=auto'
+alias xzfgrep='xzfgrep --color=auto'
+alias xzgrep='xzgrep --color=auto'
+alias zegrep='zegrep --color=auto'
+alias zfgrep='zfgrep --color=auto'
+alias zgrep='zgrep --color=auto'
