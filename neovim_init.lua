1a9ec758080d51e134acdf8a8eb3d61085b18ebb
Thomas Bratt <thomas.bratt@gmail.com>
2023-06-17 09:26:57 +0100

    Working vim configuration

--- neovim_init.lua
+++ neovim_init.lua
@@ -13,8 +13,13 @@ end
 local packer_bootstrap = ensure_packer()
 
 require('packer').startup(function(use)
+
 	-- Packer plugin manager
 	use 'wbthomason/packer.nvim'
+
+	-- explorer like tree panel
+	use 'nvim-tree/nvim-tree.lua'
+	use 'nvim-tree/nvim-web-devicons'
 
 	-- Colorschemes
 	use 'ellisonleao/gruvbox.nvim'
@@ -22,17 +27,30 @@ require('packer').startup(function(use)
 	use 'projekt0n/github-nvim-theme'
 	use 'rose-pine/neovim'
 	use 'shaunsingh/nord.nvim'
-
+	
 	-- Transparent backgrounds
 	use 'xiyaowong/transparent.nvim'
 
-	-- Rust
-	use 'neovim/nvim-lspconfig'
-	use 'simrat39/rust-tools.nvim'
+	-- Language server
+	use 'williamboman/mason.nvim'    
+	use 'williamboman/mason-lspconfig.nvim'
+    use 'neovim/nvim-lspconfig' 
+    use 'simrat39/rust-tools.nvim'
 
-	-- Debugging
-	use 'nvim-lua/plenary.nvim'
-	use 'mfussenegger/nvim-dap'
+    -- Completion framework:
+    use 'hrsh7th/nvim-cmp' 
+
+    -- LSP completion source:
+    use 'hrsh7th/cmp-nvim-lsp'
+
+    -- Useful completion sources:
+    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-nvim-lsp-signature-help'
+    use 'hrsh7th/cmp-vsnip'                             
+    use 'hrsh7th/cmp-path'                              
+    use 'hrsh7th/cmp-buffer'                            
+    use 'hrsh7th/vim-vsnip'
+
 end)
 
 -- Automatically set up your configuration after cloning packer.nvim
@@ -40,6 +58,25 @@ end)
 if packer_bootstrap then
 	require('packer').sync()
 end
+
+-- nvim tree setup
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
+})
 
 -- Set colorscheme
 vim.cmd('colorscheme github_dark')
@@ -56,7 +93,20 @@ vim.cmd('colorscheme github_dark')
 -- :TransparentToggle-
 require('transparent').setup()
 
--- Set up rust tools
+-- Mason Setup
+-- :MasonInstall rust-analyzer codelldb
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "£",
+            package_pending = "£",
+            package_uninstalled = "£",
+        },
+    }
+})
+require("mason-lspconfig").setup()
+
+-- Rust tools
 local rt = require("rust-tools")
 rt.setup({
   server = {
@@ -69,6 +119,115 @@ rt.setup({
   },
 })
 
+-- LSP Diagnostics Options Setup 
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({name = 'DiagnosticSignError', text = ''})
+sign({name = 'DiagnosticSignWarn', text = ''})
+sign({name = 'DiagnosticSignHint', text = ''})
+sign({name = 'DiagnosticSignInfo', text = ''})
+
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+})
+
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+
+--Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not select, force to select one from the menu
+-- shortness: avoid showing extra messages when using completion
+-- updatetime: set updatetime for CursorHold
+vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true}
+vim.api.nvim_set_option('updatetime', 300) 
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error 
+-- Show inlay_hints more frequently 
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+-- Completion Plugin Setup
+local cmp = require'cmp'
+cmp.setup({
+  -- Enable LSP snippets
+  snippet = {
+    expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    -- Add tab support
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    })
+  },
+  -- Installed sources:
+  sources = {
+    { name = 'path' },                              -- file paths
+    { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
+    { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
+    { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
+    { name = 'buffer', keyword_length = 2 },        -- source current buffer
+    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
+    { name = 'calc'},                               -- source for math calculation
+  },
+  window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+      fields = {'menu', 'abbr', 'kind'},
+      format = function(entry, item)
+          local menu_icon ={
+              nvim_lsp = 'λ',
+              vsnip = '⋗',
+              buffer = 'Ω',
+              path = '🖫',
+          }
+          item.menu = menu_icon[entry.source.name]
+          return item
+      end,
+  },
+})
+
+
+-- Vim editor settings
 vim.o.expandtab = true
 vim.o.number = true    
 vim.o.shiftwidth = 4
